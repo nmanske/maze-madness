cb3cba8327b7fb5822606da8295a7b1bf87e6fe5
Nathan Manske <nathan.manske@utexas.edu>
2015-10-21 14:12:17 -0500

    Added Express.io support, testesd Quintus open-source starter code

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.listen(80);
+app.use(express.static(__dirname + '/public'));
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+app.get('/', function(req, res){
+  res.sendFile('/index.html');
 });
+
+server.listen(8080);
+console.log("Listening on port 8080");
