6bba93bf80e428affc0d301e2f18df973e39e286
Nathan Manske <nathan.manske@utexas.edu>
2015-10-29 17:15:34 -0500

    Changed enemy player color, slight code cleanup, added (broken) second level

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile('/index.html');
+app.get("/", function(req, res){
+  res.sendFile("/index.html");
 });
 
 var playerCount = 0;
 var id = 0;
 
-io.on('connection', function (socket) {
+io.on("connection", function (socket) {
   playerCount++;
   id++;
 
   setTimeout(function () {
-    socket.emit('connected', { playerId: id });
-    io.emit('count', { playerCount: playerCount });
+    socket.emit("connected", { playerId: id });
+    io.emit("count", { playerCount: playerCount });
   }, 1500);
 
-  socket.on('disconnect', function () {
+  socket.on("disconnect", function () {
     playerCount--;
-    io.emit('count', { playerCount: playerCount });
+    io.emit("count", { playerCount: playerCount });
   });
 
-  socket.on('update', function (data) {
-    socket.broadcast.emit('updated', data);
+  socket.on("update", function (data) {
+    socket.broadcast.emit("updated", data);
   });
 });
 
