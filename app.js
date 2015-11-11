0197e53562357b0d0985ac2cdc1014e9571f6059
aksharpatel <akshar@izuna.net>
2015-11-11 05:35:55 -0600

    Updated contact_me.php and repository

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
+nicknames=[];
 
 app.use(express.static(__dirname + "/public"));
 
@@ -12,24 +13,62 @@ app.get("/", function(req, res){
 var playerCount = 0;
 var id = 0;
 
-io.on("connection", function (socket) {
+io.on("connection", function (socket) 
+{
   playerCount++;
   id++;
 
-  setTimeout(function () {
+  setTimeout(function () 
+  {
     socket.emit("connected_game", { playerId: id });
     io.emit("count", { playerCount: playerCount });
   }, 1500);
 
-  socket.on("disconnect", function () {
+  socket.on("disconnect", function () 
+  {
     playerCount--;
     io.emit("count", { playerCount: playerCount });
   });
 
-  socket.on("update", function (data) {
+  socket.on("update", function (data) 
+  {
     socket.broadcast.emit("updated", data);
   });
+
 });
+
+
+io.sockets.on('connection', function(socket)
+{
+  socket.on('new user', function(data, callback)
+  {
+    if (nicknames.indexOf(data) != -1)
+    {
+      callback(false);
+    }
+     else{
+      callback(true);
+      socket.nickname = data;
+      nicknames.push(socket.nickname);
+      updateNicknames();
+    }
+  });
+});
+
+
+function updateNicknames()
+{
+    io.sockets.emit('usernames', nicknames);
+}
+
+io.sockets.on('connection', function(socket)
+{
+  socket.on('send message', function(data)
+  {
+    io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+  });
+});
+
 
 server.listen(8080);
 console.log("Listening on port 8080");
